c96813218dd3d0810f79780fa3bc2eabddd92786
Chris Savage <savage@Chriss-MacBook-Pro.local>
2017-06-12 00:24:25 -0400

    added dynamic html templating using ejs

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@ import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
@@ -18,6 +19,7 @@ export default {
     filename: '[name].[chunkhash].js'
   },
   plugins: [
+    new ExtractTextPlugin('[name].[contenthash].css'),
     //for cache busting
     new WebpackMd5Hash(),
     //chunk common files so they are bundled seperately
@@ -41,7 +43,9 @@ minifyJs:true,
 minifyCSS:true,
 minifyURLs:true
       },
-      inject: true
+      inject: true,
+      //custom tokens here
+      trackJSToken:"test"
     }),
     //remove duplicate packages
     new webpack.optimize.DedupePlugin(),
@@ -52,7 +56,7 @@ minifyURLs:true
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
