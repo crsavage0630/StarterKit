18344711c7fa9f2a3344931906a2ed298b78081a
Chris Savage <savage@Chriss-MacBook-Pro.local>
2017-06-11 21:32:29 -0400

    add http mock framework

---
+++ src/api/userApi.js
@@ -0,0 +1,35 @@
+import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
+
+/* eslint-disable no-console */
+export function getUsers(){
+  return get('users');
+}
+
+export function deleteUser(id){
+  return del(`users/${id}`);
+}
+
+function get(url){
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url){
+  const request = new Request(baseUrl + url, {
+    method:'DELETE'
+  })
+  return fetch(request).then(onSuccess, onError);
+}
+
+
+function onSuccess(response)
+{
+  return response.json();
+}
+
+function onError(error)
+{
+  return console.log(error);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import './index.css';
-//import numeral from 'numeral';
+import {getUsers, deleteUser} from'./api/userApi'
 
-//const value =numeral(1000).format('$0,0.00');
-//console.log(`i would pay ${value} for this course`);
+
+getUsers().then(result => {
+  let usersBody = "";
+  result.forEach(user => {
+    usersBody +=`<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>`;
+    });
+    global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName("deleteUser");
+
+    Array.from(deleteLinks, link => {
+      link.onclick = function(event) {
+          const elm = event.target;
+          event.preventDefault();
+          deleteUser(elm.attributes["data-id"].value);
+          const row =elm.parentNode.parentNode;
+          row.parentNode.removeChild(row);
+      };
+    });
+
+
+});
